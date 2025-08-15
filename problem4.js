ec3b17c4d1fa976c0aefb3c2b710bba37a342d33
Sumi Akter <sumiwdcse@gmail.com>
2025-08-15 18:34:11 +0600

    problem 4 solved

--- problem4.js
+++ problem4.js
@@ -4,12 +4,13 @@ function  isSame(arr1 , arr2 ) {
             return 'Invalid' ;
           }
           else if (arr1.length !== arr2.length){
-            return 'false' ;
+            return false ;
           }
           for (let i = 0; i< arr1.length; i++){
-            if( arr1[i] == arr2[i]){
-                return 'true' ;
+            if( arr1[i] !== arr2[i]){
+                return false ;
             }
           }
+          return true;
 }
-console.log(isSame([1, 2, 3] , [1 , 3 , 3]))
+console.log(isSame([1 , "4" , 4] ,[1 , 4  , 4]));
